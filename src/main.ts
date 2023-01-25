689cbca20607808d9f40a9048573ae360cda4277
codeinvisible <lamasandesh400@gmail.com>
2023-01-25 21:24:41 +0545

    cors error handled

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { SwaggerModule } from '@nestjs/swagger/dist';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('api', { exclude: ['/'] });
   const config = new DocumentBuilder()
     .setTitle('Ecommerce')
