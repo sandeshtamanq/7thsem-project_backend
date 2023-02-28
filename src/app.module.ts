a239a2cd681db7a6b9408b8849aec843351b1062
codeinvisible <lamasandesh400@gmail.com>
2023-02-28 16:27:03 +0545

    supabase db url added

--- src/app.module.ts
+++ src/app.module.ts
@@ -19,14 +19,14 @@ import { CartModule } from './cart/cart.module';
       useFactory: () => {
         return {
           type: 'postgres',
-          host: process.env.DB_HOST || 'localhost',
-          port: parseInt(process.env.DB_PORT),
-          username: process.env.DB_USER,
-          password: process.env.DB_PASSWORD,
-          database: process.env.DB_NAME,
+          // host: process.env.DB_HOST || 'localhost',
+          // port: parseInt(process.env.DB_PORT),
+          // username: process.env.DB_USER,
+          // password: process.env.DB_PASSWORD,
+          // database: process.env.DB_NAME,
           // entities: [__dirname + './**/models/entity/*.entity{.ts,.js}'],
-          entities: [UserEntity, ProductEntity],
-          // url: process.env.DATABASE_URL,
+          // entities: [UserEntity, ProductEntity],
+          url: process.env.DATABASE_URL,
           autoLoadEntities: true,
           synchronize: true,
         };
