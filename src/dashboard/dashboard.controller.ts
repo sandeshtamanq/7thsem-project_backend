94235ce41a33828d154860888020b2f1711c16e0
codeinvisible <lamasandesh400@gmail.com>
2023-05-17 23:58:05 +0545

    dashboard api guard added

--- src/dashboard/dashboard.controller.ts
+++ src/dashboard/dashboard.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
+import { JwtAuthGuard } from 'src/auth/guard/jwt.guard';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { hasRoles } from 'src/auth/decorator/roles.decorator';
+import { UserRoles } from 'src/auth/models/interface/user.roles';
 
 @Controller('dashboard')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@hasRoles(UserRoles.ADMIN)
 export class DashboardController {
   constructor(private dashboardService: DashboardService) {}
 
